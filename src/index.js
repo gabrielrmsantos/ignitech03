c7c05f292593571407a428ba9fad16a17ed2d7f3
Gabriel Roberto <gabrielrmsantos@outlook.com>
2021-03-13 15:35:38 -0300

    desafios completos

--- src/index.js
+++ src/index.js
@@ -23,18 +23,21 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  delete updatedRepository.likes;
 
-  if (repositoryIndex < 0) {
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0)
     return response.status(404).json({ error: "Repository not found" });
-  }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -48,9 +51,8 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0)
     return response.status(404).json({ error: "Repository not found" });
-  }
 
   repositories.splice(repositoryIndex, 1);
 
@@ -62,13 +64,12 @@ app.post("/repositories/:id/like", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex < 0)
     return response.status(404).json({ error: "Repository not found" });
-  }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
